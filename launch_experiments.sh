93640b9d9be889d2ddb770243ed51d2727fd9857
Prasoon Sinha <prasoon.sinha@utexas.edu>
2024-04-25 04:46:47 +0000

    Add a100 results for combos of size 1, 2, 3

--- launch_experiments.sh
+++ launch_experiments.sh
@@ -2,22 +2,24 @@
 
 mixes=(
 # "diffusion-1"
-# "whisper-1"
-# "bert-1"
-# "gpt-1"
-# "bert-1 whisper-1"
-# "bert-1 gpt-1"
-# "bert-1 diffusion-1"
-# "whisper-1 gpt-1"
-# "whisper-1 diffusion-1"
-# "gpt-1 diffusion-1"
+"whisper-1"
+"bert-1"
+"gpt-1"
+"bert-1 whisper-1"
+"bert-1 gpt-1"
+"bert-1 diffusion-1"
+"whisper-1 gpt-1"
+"whisper-1 diffusion-1"
+"gpt-1 diffusion-1"
 "bert-1 whisper-1 gpt-1"
 "bert-1 whisper-1 diffusion-1"
+"bert-1 gpt-1 diffusion-1"
+"whisper-1 gpt-1 diffusion-1"
 )
 
 for ((i=0; i<${#mixes[@]}; i++)); do
     mix="${mixes[$i]}"
-    cmd="./run.sh --device-type 4090 --device-id 0 --modes tm --duration 300 ${mix}"
+    cmd="./run.sh --device-type a100 --device-id 0 --modes tm --duration 300 ${mix}"
     echo "${cmd}"
     eval $cmd
 done
